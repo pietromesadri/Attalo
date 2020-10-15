ed5772c03bfca78bb2a24296f855ab179dd183ed
Pietro Nardelli Mesadri <pietromesadri@gmail.com>
2020-10-15 11:58:38 +0200

    Linux Version init

---
+++ CMakeFiles/Attalo.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pietro/Documents/Programming/Attalo/lib/src/glad.c" "/home/pietro/Documents/Programming/Attalo/CMakeFiles/Attalo.dir/lib/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pietro/Documents/Programming/Attalo/src/displayManager.cpp" "/home/pietro/Documents/Programming/Attalo/CMakeFiles/Attalo.dir/src/displayManager.cpp.o"
+  "/home/pietro/Documents/Programming/Attalo/src/input.cpp" "/home/pietro/Documents/Programming/Attalo/CMakeFiles/Attalo.dir/src/input.cpp.o"
+  "/home/pietro/Documents/Programming/Attalo/src/main.cpp" "/home/pietro/Documents/Programming/Attalo/CMakeFiles/Attalo.dir/src/main.cpp.o"
+  "/home/pietro/Documents/Programming/Attalo/src/renderer.cpp" "/home/pietro/Documents/Programming/Attalo/CMakeFiles/Attalo.dir/src/renderer.cpp.o"
+  "/home/pietro/Documents/Programming/Attalo/src/stb_image.cpp" "/home/pietro/Documents/Programming/Attalo/CMakeFiles/Attalo.dir/src/stb_image.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Attalo.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "Attalo"
+  "Attalo.pdb"
+  "CMakeFiles/Attalo.dir/lib/src/glad.c.o"
+  "CMakeFiles/Attalo.dir/src/displayManager.cpp.o"
+  "CMakeFiles/Attalo.dir/src/input.cpp.o"
+  "CMakeFiles/Attalo.dir/src/main.cpp.o"
+  "CMakeFiles/Attalo.dir/src/renderer.cpp.o"
+  "CMakeFiles/Attalo.dir/src/stb_image.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Attalo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
